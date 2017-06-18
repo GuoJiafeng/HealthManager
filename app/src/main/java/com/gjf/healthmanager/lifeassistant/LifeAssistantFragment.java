642251319ad0b0094c3ffc00e06dc2c186e8f37e
G <gjfwyfcm@gmail.com>
2017-06-18 16:46:57 +0800

    2017/06/18 1、完成医药记录的增删查改 2、加入两个WEBVIEW
    2017/06/16
    
    1、完成体检报告的增删查改
    
    2017/04/18
    1、完成健康档案的界面切换。
    2、该界面部分细节待优化。
    2017/04/16
    1、主界面基本按照MVP框架搭建完成，已经可以显示。
    2、下一步将进行健康档案的Fragmen以及数据库开发。
    2017/04/10
    1、在第一上传的基础加上了各个xml文件。19.54
    2、加入title 20.09

--- app/src/main/java/com/gjf/healthmanager/lifeassistant/LifeAssistantFragment.java
+++ app/src/main/java/com/gjf/healthmanager/lifeassistant/LifeAssistantFragment.java
@@ -6,20 +6,42 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.gjf.healthmanager.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by BlackBeard丶 on 2017/04/10.
  */
 
-public class LifeAssistantFragment  extends Fragment {
+public class LifeAssistantFragment extends Fragment {
+    @BindView(R.id.life_assistant_fragment_webview)
+    WebView lifeAssistantFragmentWebview;
+    Unbinder unbinder;
     private View view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.life_assistant_fragment,container,false);
+        view = inflater.inflate(R.layout.life_assistant_fragment, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        lifeAssistantFragmentWebview.loadUrl("http://m.meishichina.com");
+        WebSettings webSettings = lifeAssistantFragmentWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        lifeAssistantFragmentWebview.setWebChromeClient(new WebChromeClient());
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
--- app/src/main/java/com/gjf/healthmanager/onlinesearch/OnlinSearcherFragment.java
+++ app/src/main/java/com/gjf/healthmanager/onlinesearch/OnlinSearcherFragment.java
@@ -6,20 +6,46 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.gjf.healthmanager.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by BlackBeard丶 on 2017/04/10.
  */
 
 public class OnlinSearcherFragment extends Fragment {
+
+    Unbinder unbinder;
+    @BindView(R.id.online_search_fragment_webview)
+    WebView onlineSearchFragmentWebview;
     private View view;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.online_search_fragment,container,false);
+        view = inflater.inflate(R.layout.online_search_fragment, container, false);
+
+
+        unbinder = ButterKnife.bind(this, view);
+
+        onlineSearchFragmentWebview.loadUrl("https://wapbaike.baidu.com");
+        WebSettings webSettings = onlineSearchFragmentWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        onlineSearchFragmentWebview.setWebViewClient(new WebViewClient());
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
